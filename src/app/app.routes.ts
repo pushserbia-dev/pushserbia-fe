6efdbb0359557b03d51bdd0f1464a98890127997
Dusan Perisic <dullle@live.com>
2025-05-15 09:41:14 +0200

    chore: replace lazy loading of LandingComponent with dynamic import in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './features/landing/landing.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LandingComponent,
+    loadComponent: () =>
+      import('./features/landing/landing.component').then(
+        (m) => m.LandingComponent,
+      ),
   },
   {
     path: 'placanja',
